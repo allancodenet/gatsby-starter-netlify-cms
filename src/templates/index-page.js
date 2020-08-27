d469ca8052e3746a593360815d6871ebba6fef17
allancodenet <60258407+allancodenet@users.noreply.github.com>
2020-08-28 02:58:45 +0300

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -40,7 +40,10 @@ export const IndexPageTemplate = ({
         <h1
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
-            color: 'black',
+               boxShadow:
+              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+           backgroundColor: 'rgb(255, 68, 0 , 0.3)',
+            color: 'white',
             lineHeight: '1',
             padding: '0.25em',
           }}
@@ -50,7 +53,10 @@ export const IndexPageTemplate = ({
         <h3
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
-            color: 'black',
+             boxShadow:
+              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+            backgroundColor: 'rgb(255, 68, 0 ,0.3)',
+            color: 'white',
             lineHeight: '1',
             padding: '0.25em',
           }}
