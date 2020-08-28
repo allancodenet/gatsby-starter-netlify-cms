2083013dc0a09c27b83bccbde5e62931e5a2911c
allancodenet <60258407+allancodenet@users.noreply.github.com>
2020-08-28 06:04:09 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,9 +10,9 @@ import twitter from '../img/social/twitter.svg'
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
+      <footer className="footer has-background-grey has-text-white-ter">
+        <div className="content has-text-centered has-background-grey has-text-white-ter">
+          <div className="container has-background-grey has-text-white-ter">
             <div style={{ maxWidth: '100vw' }} className="columns">
               <div className="column is-4">
                 <section className="menu">
