d34a74ca24cf029539a0cb6a51d0e1e1dbd94794
allancodenet <60258407+allancodenet@users.noreply.github.com>
2020-08-29 05:39:15 +0300

    Update Pricing.js

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -2,17 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Pricing = ({ data }) => (
-  <div className="columns" style={{
-          display: 'flex',
-        }}>
+  <div className="columns">
     {data.map((price) => (
       <div key={price.plan} className="column" >
         <section className="section">
           <h4 className="has-text-centered has-text-weight-semibold">
             {price.plan}
           </h4>
-          <h2 className="is-size-3 has-text-weight-bold has-text-grey has-text-centered">
-            ${price.price}
+          <h2 className="is-size-1 has-text-weight-bold has-text-grey has-text-centered">
+            KSH{price.price}
           </h2>
           <p className="has-text-weight-semibold">{price.description}</p>
           <ul>
