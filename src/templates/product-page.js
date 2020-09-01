948dcd1b62cef1c0953cf973e1cc949531cfcbab
allancodenet <60258407+allancodenet@users.noreply.github.com>
2020-09-01 07:45:59 +0300

    Update product-page.js

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -31,8 +31,8 @@ export const ProductPageTemplate = ({
       <h2
         className="has-text-weight-bold is-size-1"
         style={{
-          boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-          backgroundColor: '#f40',
+          boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #005746',
+          backgroundColor: '#005746',
           color: 'white',
           padding: '1rem',
         }}
